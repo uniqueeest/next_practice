c9c79d3740f032de140d569e9dbf7199d47091b9
Yoonjae Choi <dbswo9795@gmail.com>
2024-01-28 11:28:39 +0900

    fix: 컴포넌트 별 묶음으로 통일

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { Tabs } from '../components/Tabs';
-import InputBox from '../components/InputBox';
+import { Tabs, InputBox, MPBox } from '../components';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center px-5 py-6">
+    <div className="flex flex-col items-center px-5 py-6 rounded-tl-[36px] rounded-tr-[36px] shadow-wip-box">
       <Tabs defaultValue="1">
         <Tabs.List>
           <Tabs.Trigger value="1" text="전국" />
@@ -13,7 +12,8 @@ export default function Home() {
           <Tabs.Trigger value="3" text="정당별" />
         </Tabs.List>
         <Tabs.Panel value="1">
-          <InputBox containerStyle={{ marginTop: '30px' }} />
+          <InputBox containerStyle={{ margin: '30px 0' }} />
+          <MPBox />
         </Tabs.Panel>
         <Tabs.Panel value="2">
           <div>This is</div>
